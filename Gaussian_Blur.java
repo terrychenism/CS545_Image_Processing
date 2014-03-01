f2f22085df449597f5097de9fc171a2768f17e80
terrychenism <hahakuku@hotmail.com>
2014-02-28 23:29:19 -0500

    Update Gaussian_Blur.java

--- Gaussian_Blur.java
+++ Gaussian_Blur.java
@@ -42,20 +42,21 @@ public class Gaussian_Blur implements PlugInFilter {
       // System.out.println(len);
         ImageProcessor copy = orig.duplicate();
       
-        for (int v = k; v <= h -k- 1; v++) {
-            for (int u = k; u <= w - k-1; u++) {
+        //for (int v = 0; v < h ; v++) {
+           // for (int u = 0; u < w; u++) {
                 // compute filter result for position (u,v)
-                
+          for (int v = k; v <= h -k- 1; v++) {
+             for (int u = k; u <= w - k-1; u++) {     
                   double sum = 0;
                   double coeff = 0; 
                   for (int j = -k; j <= k; j++) {
                         for (int i = -k; i <= k; i++) {
-	                        int p = copy.getPixel(u + i, v+ j );
-	                        // get the corresponding filter coefficient
-	                        double c = filter[j+k]*filter[i+k];
-	                        coeff += c;
-	                        sum = sum +  c * p ;
-	                        //System.out.println(sum);
+                            int p = copy.getPixel(u + i, v+ j );
+                            // get the corresponding filter coefficient
+                            double c = filter[j+k]*filter[i+k];
+                            coeff += c;
+                            sum = sum +  c * p ;
+                            //System.out.println(sum);
                         }
                    
                   }
@@ -70,6 +71,52 @@ public class Gaussian_Blur implements PlugInFilter {
             }
             
         }
+          
+          int p; 
+          for (int v = 0; v < h  ; v++) {
+              for (int u = 0; u < w ; u++) {
+                  // four corner
+                  if(u<=k && v<=k){
+                       p = orig.get(k, k);
+                      
+                  }
+                  else if (u>=w-k-1 && v >= h- k-1){
+                      p = orig.get(w-k-1,h - k-1);
+                      
+                  }
+                  else if (u<=k && v >= h - k-1){
+                      p = orig.get(k,h - k-1);
+                      
+                  }
+                  else if (u>=w-k-1 && v<=k){
+                      p = orig.get(w-k-1,k);
+                      
+                  }
+                  
+                  //four rectangle
+                  else if(u>k && u<w-k-1 && v<k){
+                      p = orig.get(u,k);                  
+                  } 
+                  else if(u>k && u<w-k-1 && v>h - k-1){
+                      p = orig.get(u,h - k-1);                    
+                  }
+                  else if(u< k && v< h - k-1 && v>k){
+                      p = orig.get(k,v);                  
+                  }
+                  else if(u> w-k-1 && v< h - k-1 && v>k){
+                      p = orig.get(w-k-1,v);                  
+                  }
+                  //center
+                  else {
+                      p = orig.get(u,v);
+                  }
+                  
+                  orig.putPixel(u, v, p);
+                      
+              }
+              
+              
+          }
                
 
     }
